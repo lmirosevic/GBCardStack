87334984bb22aafedb45f41a947c323488ae0a74
Luka Mirosevic <luka@goonbee.com>
2012-01-15 16:45:56 +0100

    Initial Commit

---
+++ Goonbee Cardstack/GBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GBAppDelegate.h
+//  Goonbee Cardstack
+//
+//  Created by Luka Mirošević on 15/01/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Goonbee Cardstack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Goonbee Cardstack
+//
+//  Created by Luka Mirošević on 15/01/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GBAppDelegate class]));
+    }
+}

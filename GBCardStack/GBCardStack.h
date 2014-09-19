eccc07d38776e35f455686f80abbca89295951fa
Luka Mirosevic <luka@goonbee.com>
2014-09-19 13:46:27 +0200

    Added header

---
+++ GBCardStack/GBCardStack.h
@@ -0,0 +1,10 @@
+//
+//  UIViewController+GBCardStack.h
+//  Goonbee Cardstack
+//
+//  Created by Luka Mirošević on 19/09/2014.
+//  Copyright (c) 2014 Goonbee. All rights reserved.
+//
+
+#import "GBCardStackController.h"
+#import "UIViewController+GBCardStack.h"
